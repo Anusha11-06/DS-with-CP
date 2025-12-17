c70af8155d00e53c04d91656344d08bb57cbbbb4
Anusha11-06 <241801370055@cutmap.ac.in>
2025-12-17 16:09:34 +0530

    Create StaticInitialization.java

---
+++ Module-1_Array/StaticInitialization.java
@@ -0,0 +1,22 @@
+class StaticIntialization {
+    public static void main(String[] args) {
+
+        int[] numbers = {2, 5, 7, 8};
+        String[] names = {"anu", "sushmi", "pinky"};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+        int[] darr= new int[10]; // declaration or allocation space
+
+        darr[0]=1;// assign value to particular memory space 
+        darr[9]=8;
+for (int i = 0; i < darr.length; i++) {
+        System.out.println(darr[i]);
+}
+    }
+}
